c55161ffcecc6f8954cd7137e94cf15100118f49
spiegel123 <enzopro2222@gmail.com>
2025-09-11 23:04:04 +0800

    added two routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -87,6 +87,8 @@ $routes->group('doctor', function($routes) {
     $routes->get('prescriptions', 'Doctor\PrescriptionsController::index');
     $routes->get('medical-records', 'Doctor\MedicalRecordController::index');
     $routes->get('lab-results', 'Doctor\LabResultsController::index');
+    $routes->get('electronic-health-records', 'Doctor\EHRController::index');
+    $routes->get('schedule', 'Doctor\ScheduleController::index');
 });
 
 $routes->group('nurse', function($routes) {
