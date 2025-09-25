280c3bc396371ef93b0f4bdafdb71f2a83efea59
jerica08 <jericamarquez88@gmail.com>
2025-09-26 02:06:28 +0800

    deleted

--- HMS-ITE311-main/public/js/logout.js
+++
@@ -1,9 +0,0 @@
-
-        // Simple navigation functionality - removed preventDefault to allow page navigation
-
-        // Logout functionality
-        document.querySelector('.logout-btn').addEventListener('click', function() {
-            if(confirm('Are you sure you want to logout?')) {
-                window.location.href = '/auth/logout';
-            }
-        });
